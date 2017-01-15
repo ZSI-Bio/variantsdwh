21735be79951a1ffe3300c834fec94ed309647e1
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2017-01-15 22:49:11 +0100

    Query execution engine

--- genomic-dwh-benchmark/src/main/scala/pl/edu/pw/ii/zsibio/dwh/benchmark/ExecuteStatement.scala
+++ genomic-dwh-benchmark/src/main/scala/pl/edu/pw/ii/zsibio/dwh/benchmark/ExecuteStatement.scala
@@ -3,6 +3,7 @@ package pl.edu.pw.ii.zsibio.dwh.benchmark
 import org.rogach.scallop.ScallopConf
 import pl.edu.pw.ii.zsibio.dwh.benchmark.dao.JDBCDriver.JDBCDriver
 import pl.edu.pw.ii.zsibio.dwh.benchmark.dao.{JDBCConnection, JDBCDriver}
+import pl.edu.pw.ii.zsibio.dwh.benchmark.utils.QueryExecutorWithLogging
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -18,40 +19,52 @@ object ExecuteStatement {
 
       banner("Usage: ...")
 
-      val dbName = opt[String](required = false, descr = "Database name in HiveMetastore" )
-      val useHiveOrc = opt[Boolean]("useHiveOrc",required = false, descr = "Create tables in Hive stored as ORC file format" )
-      val useHivePq = opt[Boolean](required = false, descr = "Create tables in Hive stored as Parquet file format" )
+      //val dbName = opt[String](required = false, descr = "Database name in HiveMetastore" )
+      val useHive = opt[Boolean]("useHive",required = false, descr = "Create tables in Hive stored")
+      val storageType = opt[String]("storageType",required = true, descr = "Storage type parquet|orc|kudu")
       val useKudu = opt[Boolean]("useKudu",required = false, descr = "Create tables in Kudu" )
       val usePresto = opt[Boolean]("usePresto",required = false, descr = "Create tables in Presto" )
-      val sparkConnstring =opt[String](required = false, descr = "Connection string for SparlSQL Server" )
-      val hiveConnstring =opt[String]("hiveConnstring",required = false, descr = "Connection string for Hive Server" )
-      val prestoConnstring =opt[String]("prestoConnstring",required = false, descr = "Connection string for Presto Server" )
-      val impalaConnstring =opt[String]("impalaConnstring",required = false, descr = "Connection string for Impala Server" )
+      val connString =opt[String]("connString",required = false, descr = "Connection string for SparlSQL Server" )
       val kuduMaster =opt[String]("kuduMaster",required = false, descr = "Kudu Master URL" )
       val compression =opt[String]("compression",required = false, default= Some("gzip"), descr = "Compression algorithm gzip|snappy|none" )
       val username =opt[String]("username",required = false, descr = "Username" )
       val password =opt[String]("password",required = false, descr = "Password" )
-      val queryFile =opt[String]("queryFile",required = false, descr = "A file containing a select statement in YAML format" )
-      val ddlFile =opt[String]("ddlFile",required = false, descr = "A file containing a DDL statement in YAML format" )
-
+      val queryFile =opt[String]("queryFile",required = true, descr = "A file containing a select statement in YAML format" )
+      val logFile =opt[String]("logFile",required = true, descr = "A file for storing timing results" )
       verify()
     }
     val jdbcConfArray = new ArrayBuffer[(JDBCDriver, String)]()
 
-    if( (Conf.useHiveOrc() ||  Conf.useHivePq()) && Conf.hiveConnstring.toOption != None)
-      jdbcConfArray.append((JDBCDriver.HIVE,Conf.hiveConnstring() ) )
+    if( Conf.useHive() && Conf.connString.toOption != None)
+      jdbcConfArray.append((JDBCDriver.HIVE,Conf.connString() ) )
 
-    if (Conf.usePresto() && Conf.prestoConnstring.toOption != None)
-      jdbcConfArray.append((JDBCDriver.PRESTO,Conf.prestoConnstring()) )
+    if (Conf.usePresto() && Conf.connString.toOption != None)
+      jdbcConfArray.append((JDBCDriver.PRESTO,Conf.connString()) )
 
-    if (Conf.useKudu() && Conf.impalaConnstring.toOption != None)
-      jdbcConfArray.append((JDBCDriver.IMPALA,Conf.impalaConnstring() ) )
+    if (Conf.useKudu() && Conf.connString.toOption != None)
+      jdbcConfArray.append((JDBCDriver.IMPALA,Conf.connString() ) )
 
     jdbcConfArray.map( jobConf=> {
         val conn = new JDBCConnection()
-        conn.open(jobConf._1,jobConf._2,Conf.username(),Conf.password() )
-        val res = conn.executeQuery("SELECT 1", true)
-        println(s"${jobConf._1.toString}: ${res.timing.get.getTiming()}")
+        conn.open(jobConf._1,jobConf._2)
+        Conf.queryFile.toOption match{
+
+          case Some(file) => {
+            def replaceVars(property:String) ={
+              property
+                .replaceAll("\\{\\{DATA_FORMAT\\}\\}",Conf.storageType().toUpperCase)
+                .replaceAll("\\{\\{DB_NAME\\}\\}",Conf.connString().split('/').last.toUpperCase)
+            }
+            val query = QueryExecutorWithLogging.parseQuertYAML(file)
+            val queryWithReplacedVars = query.copy(
+              queryId = replaceVars(query.queryId),
+              queryDesc = replaceVars(query.queryDesc),
+              statement = replaceVars(query.statement)
+                           )
+            QueryExecutorWithLogging.runStatement(queryWithReplacedVars,conn,Conf.logFile())
+          }
+          case _ => None
+        }
         conn.close
       }
      )
--- genomic-dwh-benchmark/src/main/scala/pl/edu/pw/ii/zsibio/dwh/benchmark/utils/QueryExecutorWithLogging.scala
+++ genomic-dwh-benchmark/src/main/scala/pl/edu/pw/ii/zsibio/dwh/benchmark/utils/QueryExecutorWithLogging.scala
@@ -1,31 +1,62 @@
 package pl.edu.pw.ii.zsibio.dwh.benchmark.utils
 
-import pl.edu.pw.ii.zsibio.dwh.benchmark.dao.JDBCConnection
+import java.io.{File, FileOutputStream, PrintWriter}
+import java.util.Calendar
+
+import pl.edu.pw.ii.zsibio.dwh.benchmark.dao.{JDBCConnection, QueryResult}
 import net.jcazevedo.moultingyaml._
 import net.jcazevedo.moultingyaml.DefaultYamlProtocol
 import net.jcazevedo.moultingyaml.DefaultYamlProtocol._
+import org.apache.log4j.Logger
+import pl.edu.pw.ii.zsibio.dwh.benchmark.dao.JDBCDriver.Value
+import pl.edu.pw.ii.zsibio.dwh.benchmark.utils.QueryType.QueryType
 /**
   * Created by marek on 15.01.17.
   */
 
-case class Query(queryId:String,queryType:String, queryDesc:String,query:String)
+case class Query(queryId:String, queryType:String, queryEngine:String, queryDesc:String, statement:String)
+
+object QueryType extends Enumeration {
+
+  type QueryType = Value
+  val SELECT, CREATE, UPDATE  = Value
+
+
+}
 object QueryExecutorWithLogging {
-
+  val log = Logger.getLogger("pl.edu.pw.ii.zsibio.dwh.benchmark.utils.QueryExecutorWithLogging")
   object QueryYamlProtocol extends DefaultYamlProtocol {
-    implicit val queryFormat = yamlFormat4(Query)
+    implicit val queryFormat = yamlFormat5(Query)
   }
 
 
-  def runStatement(file:String,conn:JDBCConnection) = {
-    val query = parseQuertYAML(file)
+  def runStatement(query: Query,conn:JDBCConnection,logFile:String) = {
+    log.info(s"Running ${query.queryId} ... using ${query.queryEngine} engine")
+    log.debug(s"Executing query: ${query.statement}")
+    query.queryType.toLowerCase() match {
+      case "select" => logQuery(conn, query, logFile)
+      case _ => conn.executeUpdate(query.statement)
+    }
+
 
   }
 
-  private def parseQuertYAML(file:String) : Query ={
+  def parseQuertYAML(file:String) : Query ={
     val lines = scala.io.Source.fromFile(file).mkString
     val yml = lines.stripMargin.parseYaml
     import QueryYamlProtocol._
     yml.convertTo[Query]
+  }
+
+  private def logQuery(conn:JDBCConnection,query: Query, logFile:String) ={
+    val rs = conn.executeQuery(query.statement,true)
+    rs.rs.next()
+    val result = s"${Calendar.getInstance().getTime().toString},${query.queryId},${query.queryEngine},${rs.timing.get.getTiming()}"
+    log.info(s"Result: ${result}")
+    val writer = new PrintWriter(new FileOutputStream(new File(logFile),true))
+    writer.write(result)
+
+
   }
 
 }
