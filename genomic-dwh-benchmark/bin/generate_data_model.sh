9e86260642c996e762288436da37976ac18d2fe2
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2017-01-22 10:40:27 +0100

    build.sbt merge fixes

--- genomic-dwh-benchmark/bin/generate_data_model.sh
+++ genomic-dwh-benchmark/bin/generate_data_model.sh
@@ -1,3 +1,23 @@
 #!/bin/bash -x
 
+
+#create Impala and Kudu data model
 java -cp target/scala-2.10/genomic-dwh-benchmark-assembly-1.0.jar pl.edu.pw.ii.zsibio.dwh.benchmark.CreateDataModelJob --help
+
+#create Hive data model using Parquet file format
+
+
+#create Hive data model using ORC file format
+
+
+#load data into Hive tables
+
+
+#load data into Kudu tables
+
+
+#compute agregated tables in Hive
+
+
+#compute agregated tables in Kudu
+
