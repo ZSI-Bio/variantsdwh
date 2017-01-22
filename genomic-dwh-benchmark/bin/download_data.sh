84f2cee688e4c56803296a7ec71967337a24dad4
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2017-01-22 23:18:43 +0100

    Download script init (files on gdrive)

---
+++ genomic-dwh-benchmark/bin/download_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+#dims
+wget "https://drive.google.com/uc?export=download&id=0B6Ngq87bjm-4c0hFZW9pdnZkdDQ" -O dim_geography.gz
+
+wget "https://drive.google.com/uc?export=download&id=0B6Ngq87bjm-4WnJGMVYzbm9MSU0" -O dim_genomic_position_refseq.gz
+
+wget "https://drive.google.com/uc?export=download&id=0B6Ngq87bjm-4NThxMTlqYllURkE" -O dim_genomic_position_ensembl.gz
+
+wget "https://drive.google.com/uc?export=download&id=0B6Ngq87bjm-4emtPLW1HSEN6U1U" -O dim_disease.gz
--- genomic-dwh-benchmark/bin/generate_data_model.sh
+++ genomic-dwh-benchmark/bin/generate_data_model.sh
@@ -1,14 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash
 
 
 #create Impala and Kudu data model
-java -cp target/scala-2.10/genomic-dwh-benchmark-assembly-1.0.jar pl.edu.pw.ii.zsibio.dwh.benchmark.CreateDataModelJob --help
+java -cp target/scala-2.10/genomic-dwh-benchmark-assembly-1.0.jar pl.edu.pw.ii.zsibio.dwh.benchmark.ExecuteStatement --useImpala --queryDir ../sql/impala/ddl --storageType kudu
 
 #create Hive data model using Parquet file format
-
+java -cp target/scala-2.10/genomic-dwh-benchmark-assembly-1.0.jar pl.edu.pw.ii.zsibio.dwh.benchmark.ExecuteStatement --useHive --queryDir ../sql/hive/ddl --storageType parquet
 
 #create Hive data model using ORC file format
-
+java -cp target/scala-2.10/genomic-dwh-benchmark-assembly-1.0.jar pl.edu.pw.ii.zsibio.dwh.benchmark.ExecuteStatement --useHive --queryDir ../sql/hive/ddl --storageType orc
 
 #load data into Hive tables
 
