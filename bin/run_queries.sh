efac1189b3a4e3d014f6e61033efef2b878744a1
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2017-02-22 17:21:51 +0100

    Shell scripts refactoring + configuration

--- bin/run_queries.sh
+++ bin/run_queries.sh
@@ -1,5 +1,26 @@
 #!/bin/bash -x
+
+for i in "$@"
+do
+case $i in
+    -d=*|--dbname=*)
+    DB_NAME="${i#*=}"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+
 PWD=$(pwd | sed 's/bin//g' | sed 's/\/$//g')
 DWH_TOOLS=`find ${PWD} -name genomic-dwh-benchmark-assembly*.jar`
-java -cp $DWH_TOOLS pl.edu.pw.ii.zsibio.dwh.benchmark.ExecuteStatement --dbName dwh_dev --useHive --storageType orc --queryDir ../sql/hive/query --logFile results.log
 
+CONF_FILE=$PWD/conf/application.conf
+
+#orc
+java -cp $DWH_TOOLS -Dconfig.file=${CONF_FILE} pl.edu.pw.ii.zsibio.dwh.benchmark.ExecuteStatement --dbName ${DB_NAME} --useHive --storageType orc --queryDir ../sql/hive/query --logFile results.log
+
+#parquet
+
+#kudu
